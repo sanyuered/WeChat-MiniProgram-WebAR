ae352654dcc4251e626fc8d606cb1d9e42822f80
AR Fashion <la2040@live.com>
2019-07-08 23:52:16 +0800

    added face tracker

--- pages/camera/camera.js
+++ pages/camera/camera.js
@@ -1,10 +1,16 @@
 
 const tracking = require('../../utils/tracking.js')
+const face = require('../../utils/face.js')
+const landmarks = require('../../utils/Landmarks.js')
+const regressor = require('../../utils/Regressor.js')
 const canvasId = 'canvas1';
+// canvas size
 const canvasWidth = 375;
-const canvasHeight = 500;
-const frameWidth = 480;
-const frameHeight = 640;
+const canvasHeight = 458;
+// frame size
+const frameWidth = 288;
+const frameHeight = 352;
+// time interval
 const intervalTime = 30;
 
 Page({
@@ -12,12 +18,110 @@ Page({
     tracker: null,
     listener: null,
     intervalId: null,
+    devicePosition: 'back',
   },
-  onLoad: function () {
-    this.initTracking();
+  onLoad: function (option) {
+    this.initTracking(option.type);
   },
   onUnload: function () {
     this.stopTacking();
+  },
+  drawByColor() {
+    var _that = this;
+    const ctx = wx.createCanvasContext(canvasId);
+    _that.tracker = new tracking.ColorTracker(['yellow', 'magenta', 'cyan']);
+    _that.tracker.on('track', function (event) {
+      if (event.data.length != 0) {
+        console.log(event);
+      }
+      // size ratio 
+      var widthRatio = canvasWidth / frameWidth;
+      var heightRatio = canvasHeight / frameHeight;
+      // draw tracking points
+      event.data.forEach(function (rect) {
+        // scale from 480x600 to 357x500
+        rect.x = Math.round(rect.x * widthRatio);
+        rect.y = Math.round(rect.y * heightRatio);
+        rect.width = Math.round(rect.width * widthRatio);
+        rect.height = Math.round(rect.height * heightRatio);
+        // custom Color
+        if (rect.color === 'custom') {
+          rect.color = _that.tracker.customColor;
+        }
+        // draw rect
+        ctx.strokeStyle = rect.color;
+        ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        // ctx.font = '11px sans-serif';
+        ctx.fillText('x:' + rect.x, rect.x + rect.width + 5, rect.y + 11);
+        ctx.fillText('y:' + rect.y, rect.x + rect.width + 5, rect.y + 22);
+      });
+      ctx.draw();
+    });
+    // start
+    _that.startTacking();
+  },
+  drawByFace() {
+    var _that = this;
+    const ctx = wx.createCanvasContext(canvasId);
+    tracking.ViolaJones.classifiers.face = face;
+    tracking.LBF.LandmarksData = landmarks;
+    tracking.LBF.RegressorData = regressor;
+    // maxNumStages depends on children of "Regressor.js"
+    tracking.LBF.maxNumStages = 1;
+
+    // new LandmarksTracker
+    _that.tracker = new tracking.LandmarksTracker();
+    _that.tracker.setInitialScale(4);
+    _that.tracker.setStepSize(2);
+    _that.tracker.setEdgesDensity(0.1);
+    _that.tracker.on('track', function (event) {
+      if (!event.data) {
+        return;
+      }
+      console.log(event.data);
+      // size ratio 
+      var widthRatio = canvasWidth / frameWidth;
+      var heightRatio = canvasHeight / frameHeight;
+      // draw rect
+      if (event.data.faces) {
+        event.data.faces.forEach(function (rect) {
+          // scale from 480x600 to 357x500
+          rect.x = Math.round(rect.x * widthRatio);
+          rect.y = Math.round(rect.y * heightRatio);
+          rect.width = Math.round(rect.width * widthRatio);
+          rect.height = Math.round(rect.height * heightRatio);
+          // draw
+          ctx.strokeStyle = '#a64ceb';
+          ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          //ctx.font = '11px sans-serif';
+          ctx.fillStyle = "#fff";
+          ctx.fillText('x:' + rect.x, rect.x + rect.width + 5, rect.y + 11);
+          ctx.fillText('y:' + rect.y, rect.x + rect.width + 5, rect.y + 22);
+        });
+      }
+
+      // draw points
+      if (event.data.landmarks) {
+        event.data.landmarks.forEach(function (landmarks) {
+          for (var landmark in landmarks) {
+            var x = landmarks[landmark][0];
+            var y = landmarks[landmark][1];
+            // scale from 480x600 to 357x500
+            x = Math.round(x * widthRatio);
+            y = Math.round(y * heightRatio);
+            // draw
+            ctx.beginPath();
+            ctx.fillStyle = "#fff";
+            ctx.arc(x,y, 1, 0, 2 * Math.PI);
+            ctx.fill();
+          }
+        });
+      }
+
+      ctx.draw();
+    });
+    // start
+    _that.startTacking();
   },
   stopTacking() {
     clearInterval(this.data.intervalId);
@@ -37,7 +141,7 @@ Page({
       console.log(message);
       wx.showToast({
         title: message,
-        icon: "none"
+        icon: 'none'
       });
       return;
     }
@@ -52,7 +156,7 @@ Page({
     // start
     listener.start();
     // process
-    var intervalId = setInterval(function () {
+    const intervalId = setInterval(function () {
       console.log('tracking');
       if (frameData) {
         _that.tracker.track(frameData,
@@ -65,44 +169,26 @@ Page({
       listener: listener,
       intervalId: intervalId,
     });
-
   },
-  initTracking() {
-    var _that = this;
-    const ctx = wx.createCanvasContext(canvasId);
-    _that.tracker = new tracking.ColorTracker(['yellow', 'magenta', 'cyan']);
-    _that.tracker.on('track', function (event) {
-      if (event.data.length != 0) {
-        console.log(event);
-      }
-      
-      var widthRatio = canvasWidth / frameWidth;
-      var heightRatio = canvasHeight / frameHeight;
+  initTracking(type) {
+    if (type === 'color') {
+      this.drawByColor();
+    }
+    else if (type === 'face') {
+      this.drawByFace();
+    }
+  },
+  changeDirection() {
+    var status = this.data.devicePosition;
 
-      // draw tracking points
-      event.data.forEach(function (rect) {
-        // scale from 480x600 to 357x500
-        rect.x = Math.round(rect.x * widthRatio);
-        rect.y = Math.round(rect.y * heightRatio);
-        rect.width = Math.round(rect.width * widthRatio);
-        rect.height = Math.round(rect.height * heightRatio);
-       
-        // custom Color
-        if (rect.color === 'custom') {
-          rect.color = _that.tracker.customColor;
-        }
-        // draw rect
-        ctx.strokeStyle = rect.color;
-        ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
-        // ctx.font = '11px sans-serif';
-        ctx.fillText('x:' + rect.x, rect.x + rect.width + 5, rect.y + 11);
-        ctx.fillText('y:' + rect.y, rect.x + rect.width + 5, rect.y + 22);
-      });
-      ctx.draw();
+    if (status === 'back') {
+      status = 'front';
+    } else {
+      status = 'back';
+    }
+    this.setData({
+      devicePosition: status,
     });
-    
-    // start
-    _that.startTacking();
   },
   error(e) {
     console.log(e.detail);
--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -1,5 +1,8 @@
 
 const tracking = require('../../utils/tracking.js')
+const face = require('../../utils/face.js')
+const landmarks = require('../../utils/Landmarks.js')
+const regressor = require('../../utils/Regressor.js')
 const canvasId = 'canvas1';
 const canvasWidth = 375;
 const canvasHeight = 450;
@@ -10,21 +13,22 @@ Page({
     isRunning: true,
     tracker: null,
     tempImagePath: null,
+    devicePosition: 'back',
   },
-  onLoad: function () {
-    this.initTracking();
+  onLoad: function (option) {
+    this.initTracking(option.type);
   },
-  initTracking(){
+  drawByColor() {
     var _that = this;
     const ctx = wx.createCanvasContext(canvasId);
-    _that.tracker = new tracking.ColorTracker(['yellow','magenta','cyan']);
+
+    _that.tracker = new tracking.ColorTracker(['yellow', 'magenta', 'cyan']);
     _that.tracker.on('track', function (event) {
       if (event.data.length != 0) {
         console.log(event);
       }
       // draw photo on canvas
-      ctx.drawImage(_that.data.tempImagePath, 0, 0,canvasWidth,canvasHeight);
-
+      ctx.drawImage(_that.data.tempImagePath, 0, 0, canvasWidth, canvasHeight);
       // draw tracking points
       event.data.forEach(function (rect) {
         if (rect.color === 'custom') {
@@ -39,6 +43,73 @@ Page({
       ctx.draw();
     });
   },
+  drawByFace() {
+    var _that = this;
+    const ctx = wx.createCanvasContext(canvasId);
+    tracking.ViolaJones.classifiers.face = face;
+    tracking.LBF.LandmarksData = landmarks;
+    tracking.LBF.RegressorData = regressor;
+    // maxNumStages depends on children of "Regressor.js"
+    tracking.LBF.maxNumStages = 1;
+
+    // new LandmarksTracker
+    _that.tracker = new tracking.LandmarksTracker();
+    _that.tracker.setInitialScale(4);
+    _that.tracker.setStepSize(2);
+    _that.tracker.setEdgesDensity(0.1);
+    _that.tracker.on('track', function (event) {
+      if (!event.data) {
+        return;
+      }
+
+      if (event.data.faces.length === 0) {
+        var message = 'No results found.';
+        console.log(message);
+        wx.showToast({
+          title: message,
+          icon: 'none'
+        });
+      }
+      console.log(event.data);
+
+      // draw photo on canvas
+      ctx.drawImage(_that.data.tempImagePath, 0, 0, canvasWidth, canvasHeight);
+
+      // draw rect
+      if (event.data.faces) {
+        event.data.faces.forEach(function (rect) {
+          ctx.strokeStyle = '#a64ceb';
+          ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          //ctx.font = '11px sans-serif';
+          ctx.fillStyle = "#fff";
+          ctx.fillText('x: ' + rect.x, rect.x + rect.width + 5, rect.y + 11);
+          ctx.fillText('y: ' + rect.y, rect.x + rect.width + 5, rect.y + 22);
+        });
+      }
+
+      // draw points
+      if (event.data.landmarks) {
+        event.data.landmarks.forEach(function (landmarks) {
+          for (var landmark in landmarks) {
+            ctx.beginPath();
+            ctx.fillStyle = "#fff";
+            ctx.arc(landmarks[landmark][0], landmarks[landmark][1], 1, 0, 2 * Math.PI);
+            ctx.fill();
+          }
+        });
+      }
+
+      ctx.draw();
+    });
+  },
+  initTracking(type) {
+    if (type === 'color') {
+      this.drawByColor();
+    }
+    else if (type === 'face') {
+      this.drawByFace();
+    }
+  },
   takePhoto() {
     var _that = this;
     const context = wx.createCameraContext();
@@ -51,14 +122,15 @@ Page({
       });
       // take a photo
       context.takePhoto({
-        quality: 'medium',
+        quality: 'normal',
         success: (res) => {
+          var photoPath = res.tempImagePath;
           _that.setData({
-            tempImagePath: res.tempImagePath
+            tempImagePath: photoPath,
           });
-          console.log(res.tempImagePath);
+          console.log(photoPath);
           // draw photo on canvas
-          ctx.drawImage(res.tempImagePath, 0, 0,canvasWidth,canvasHeight);
+          ctx.drawImage(photoPath, 0, 0, canvasWidth, canvasHeight);
           // waiting for drawing
           ctx.draw(false, function () {
             // get image data from canvas
@@ -69,8 +141,9 @@ Page({
               width: canvasWidth,
               height: canvasHeight,
               success(res) {
-                // process images
-                _that.tracker.track(res.data, res.width, res.height);
+                // process image
+                _that.tracker.track(res.data, canvasWidth, canvasHeight);
+                res = null;
               }
             });
           });
@@ -85,7 +158,18 @@ Page({
       // clear canvas
       ctx.draw();
     }
+  },
+  changeDirection() {
+    var status = this.data.devicePosition;
 
+    if (status === 'back') {
+      status = 'front';
+    } else {
+      status = 'back';
+    }
+    this.setData({
+      devicePosition: status,
+    });
   },
   error(e) {
     console.log(e.detail);
