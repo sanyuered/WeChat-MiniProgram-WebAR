aa38a132d5306e3fdac4854933503f4573e45a72
AR Fashion <la2040@live.com>
2019-07-17 00:52:28 +0800

    added image tracker

--- pages/camera/camera.js
+++ pages/camera/camera.js
@@ -1,36 +1,65 @@
-
 const tracking = require('../../utils/tracking.js')
 const face = require('../../utils/face.js')
+const mouth = require('../../utils/mouth.js')
 const landmarks = require('../../utils/Landmarks.js')
 const regressor = require('../../utils/Regressor.js')
+const ImageTracker = require('../../utils/ImageTracker.js')
 const canvasId = 'canvas1';
-// canvas size
+const hiddenCanvasId = 'hiddenCanvas';
+// canvas width
 const canvasWidth = 375;
+// canvas height
 const canvasHeight = 458;
-// frame size
-const frameWidth = 288;
-const frameHeight = 352;
-// time interval
-const intervalTime = 30;
+// small frame size
+const frameWidth = 150;
+// face tracker parameter
+const initialScale = 2;
+// color tracker parameter
+const minDimension = 4;
+// pattern image url: relative url,temp url and network url.
+const patternImageUrl = '../../face.jpg';
+// pattern image width
+const patternFrameWidth = 200;
+// time interval should be greater than cost time
+const intervalTime = 350;
+//pattern image height
+var patternFrameHeight = 0;
+// color tacker, face tracker, image tracker.
+var tracker = null;
+// camera listener
+var listener = null;
+// id of setInterval
+var intervalId = null;
+// height of frame size
+var frameHeight = 0;
+// context of canvas
+var canvasContext = null;
+// context of hidden canvas
+var hiddenCanvasContext = null;
+// last face tracking result
+var lastLandmarks = null;
+// last image tracking result
+var lastTransform = null;
 
 Page({
   data: {
-    tracker: null,
-    listener: null,
-    intervalId: null,
     devicePosition: 'back',
   },
   onLoad: function (option) {
+    canvasContext = wx.createCanvasContext(canvasId);
+    hiddenCanvasContext = wx.createCanvasContext(hiddenCanvasId);
     this.initTracking(option.type);
   },
   onUnload: function () {
     this.stopTacking();
   },
-  drawByColor() {
+  drawByColorTracker() {
     var _that = this;
-    const ctx = wx.createCanvasContext(canvasId);
-    _that.tracker = new tracking.ColorTracker(['yellow', 'magenta', 'cyan']);
-    _that.tracker.on('track', function (event) {
+    //const ctx = wx.createCanvasContext(canvasId);
+    const ctx = canvasContext;
+    tracker = new tracking.ColorTracker(['yellow', 'magenta', 'cyan']);
+    tracker.setMinDimension(minDimension);
+    tracker.on('track', function (event) {
       if (event.data.length != 0) {
         console.log(event);
       }
@@ -39,14 +68,14 @@ Page({
       var heightRatio = canvasHeight / frameHeight;
       // draw tracking points
       event.data.forEach(function (rect) {
-        // scale from 480x600 to 357x500
+        // scale
         rect.x = Math.round(rect.x * widthRatio);
         rect.y = Math.round(rect.y * heightRatio);
         rect.width = Math.round(rect.width * widthRatio);
         rect.height = Math.round(rect.height * heightRatio);
         // custom Color
         if (rect.color === 'custom') {
-          rect.color = _that.tracker.customColor;
+          rect.color = tracker.customColor;
         }
         // draw rect
         ctx.strokeStyle = rect.color;
@@ -55,14 +84,50 @@ Page({
         ctx.fillText('x:' + rect.x, rect.x + rect.width + 5, rect.y + 11);
         ctx.fillText('y:' + rect.y, rect.x + rect.width + 5, rect.y + 22);
       });
+
       ctx.draw();
     });
     // start
     _that.startTacking();
   },
-  drawByFace() {
+  getPatternImage(patternImageUrl, callback) {
     var _that = this;
-    const ctx = wx.createCanvasContext(canvasId);
+    const ctx = wx.createCanvasContext(hiddenCanvasId);
+
+    wx.getImageInfo({
+      src: patternImageUrl,
+      success: function (res) {
+        // size ratio
+        patternFrameHeight = (res.height / res.width) * patternFrameWidth;
+        // for test
+        tempPatternImagePath = res.path;
+        // draw image on canvas
+        ctx.drawImage(res.path, 0, 0, frameWidth, patternFrameHeight);
+        ctx.draw(false, function () {
+          wx.canvasGetImageData({
+            canvasId: hiddenCanvasId,
+            x: 0,
+            y: 0,
+            width: frameWidth,
+            height: patternFrameHeight,
+            success(canvasRes) {
+              if (typeof callback === 'function') {
+                console.log('pattern image', canvasRes.width, canvasRes.height);
+                callback(canvasRes.data, canvasRes.width, canvasRes.height);
+              }
+            }
+          });
+        });
+      },
+      fail: function (error) {
+        console.log('getPatternImage', error);
+      }
+    });
+  },
+  drawByFaceTracker() {
+    var _that = this;
+    //const ctx = wx.createCanvasContext(canvasId);
+    const ctx = canvasContext;
     tracking.ViolaJones.classifiers.face = face;
     tracking.LBF.LandmarksData = landmarks;
     tracking.LBF.RegressorData = regressor;
@@ -70,11 +135,11 @@ Page({
     tracking.LBF.maxNumStages = 1;
 
     // new LandmarksTracker
-    _that.tracker = new tracking.LandmarksTracker();
-    _that.tracker.setInitialScale(4);
-    _that.tracker.setStepSize(2);
-    _that.tracker.setEdgesDensity(0.1);
-    _that.tracker.on('track', function (event) {
+    tracker = new tracking.LandmarksTracker();
+    tracker.setInitialScale(initialScale);
+    tracker.setStepSize(2);
+    tracker.setEdgesDensity(0.1);
+    tracker.on('track', function (event) {
       if (!event.data) {
         return;
       }
@@ -82,10 +147,16 @@ Page({
       // size ratio 
       var widthRatio = canvasWidth / frameWidth;
       var heightRatio = canvasHeight / frameHeight;
-      // draw rect
+
+      // save landmarks
       if (event.data.faces) {
-        event.data.faces.forEach(function (rect) {
-          // scale from 480x600 to 357x500
+        lastLandmarks = event.data.faces;
+      }
+
+      // draw rect
+      if (lastLandmarks) {
+        lastLandmarks.forEach(function (rect) {
+          // scale
           rect.x = Math.round(rect.x * widthRatio);
           rect.y = Math.round(rect.y * heightRatio);
           rect.width = Math.round(rect.width * widthRatio);
@@ -106,34 +177,194 @@ Page({
           for (var landmark in landmarks) {
             var x = landmarks[landmark][0];
             var y = landmarks[landmark][1];
-            // scale from 480x600 to 357x500
+            // scale
             x = Math.round(x * widthRatio);
             y = Math.round(y * heightRatio);
             // draw
             ctx.beginPath();
             ctx.fillStyle = "#fff";
-            ctx.arc(x,y, 1, 0, 2 * Math.PI);
+            ctx.arc(x, y, 1, 0, 2 * Math.PI);
             ctx.fill();
           }
         });
       }
-
       ctx.draw();
     });
     // start
     _that.startTacking();
   },
+  drawByObjectTracker() {
+    var _that = this;
+    const ctx = wx.createCanvasContext(canvasId);
+    tracking.ViolaJones.classifiers.mouth = mouth;
+    tracker = new tracking.ObjectTracker(['mouth']);
+    tracker.setInitialScale(1);
+    tracker.setStepSize(2);
+    tracker.setEdgesDensity(0.1);
+    tracker.on('track', function (event) {
+      if (event.data != null) {
+        console.log('event.data', event.data);
+      }
+      // size ratio 
+      var widthRatio = canvasWidth / frameWidth;
+      var heightRatio = canvasHeight / frameHeight;
+      // draw tracking points
+      event.data.forEach(function (rect) {
+        // scale
+        rect.x = Math.round(rect.x * widthRatio);
+        rect.y = Math.round(rect.y * heightRatio);
+        rect.width = Math.round(rect.width * widthRatio);
+        rect.height = Math.round(rect.height * heightRatio);
+        // draw
+        ctx.strokeStyle = rect.color;
+        ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        // ctx.font = '11px sans-serif';
+        ctx.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+        ctx.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      });
+      ctx.draw();
+    });
+    // start
+    _that.startTacking();
+  },
+
+  drawByImageTracker() {
+    var _that = this;
+    const ctx = wx.createCanvasContext(canvasId);
+    // get patter image
+    _that.getPatternImage(patternImageUrl, function (patternPixels, width, height) {
+      tracker = new ImageTracker(patternPixels, width, height);
+      tracker.on('track', function (event) {
+        if (event.data != null) {
+          console.log('event.data', event.data);
+        }
+        // size ratio 
+        var widthRatio = canvasWidth / frameWidth;
+        var heightRatio = canvasHeight / frameHeight;
+
+        // las transform
+        if (event.data) {
+          lastTransform = event.data.transform.data;
+        }
+        if (lastTransform) {
+          var transformArray = lastTransform;
+          var a = transformArray[0];
+          var c = transformArray[1];
+          var e = transformArray[2];
+          var b = transformArray[3];
+          var d = transformArray[4];
+          var f = transformArray[5];
+          console.log(a, b, c, d, e, f);
+          // transform canvas
+          ctx.transform(a, b, c, d, e, f);
+
+          // draw sample pattern on canvas
+          ctx.fillStyle = 'rgba(0,0,0,0.5)';
+          ctx.fillRect(0, 0, patternFrameWidth * widthRatio, patternFrameHeight * heightRatio);
+        }
+        ctx.draw();
+      });
+    });
+
+    // start
+    _that.startTacking();
+  },
   stopTacking() {
-    clearInterval(this.data.intervalId);
-    if (this.data.listener) {
-      this.data.listener.stop();
+    clearInterval(intervalId);
+    if (listener) {
+      listener.stop();
     }
+  },
+  compressPhoto(resData,
+    imageWidth,
+    imageHeight,
+    frameWidth,
+    frameHeight,
+    callback) {
+    var _that = this;
+    // image position
+    const imageX = 0;
+    const imageY = 0;
+    //const ctx = wx.createCanvasContext(hiddenCanvasId);
+    const ctx = hiddenCanvasContext;
+    wx.canvasPutImageData({
+      canvasId: hiddenCanvasId,
+      x: imageX,
+      y: imageY,
+      width: imageWidth,
+      height: imageHeight,
+      data: resData,
+      success(res) {
+        wx.canvasToTempFilePath({
+          x: imageX,
+          y: imageY,
+          width: imageWidth,
+          height: imageHeight,
+          destWidth: frameWidth,
+          destHeight: frameHeight,
+          canvasId: hiddenCanvasId,
+          success(res) {
+            if (typeof callback === 'function') {
+              callback(res.tempFilePath);
+            }
+          }
+        });
+      },
+      fail(error) {
+        console.log('canvasPutImageData', error);
+      }
+    });
+  },
+  processPhoto(resData, imageWidth, imageHeight) {
+    var _that = this;
+    //const ctx = wx.createCanvasContext(hiddenCanvasId);
+    const ctx = hiddenCanvasContext;
+    // image position
+    const imageX = 0;
+    const imageY = 0;
+    // size ratio
+    frameHeight = (imageHeight / imageWidth) * frameWidth;
+    // option: compress image
+    _that.compressPhoto(resData,
+      imageWidth,
+      imageHeight,
+      frameWidth,
+      frameHeight,
+      function (photoPath) {
+        // draw image on hidden canvas
+        ctx.drawImage(photoPath, imageX, imageY, frameWidth, frameHeight);
+        // waiting for drawing
+        ctx.draw(false, function () {
+          // get image data from hidden canvas
+          wx.canvasGetImageData({
+            canvasId: hiddenCanvasId,
+            x: imageX,
+            y: imageY,
+            width: frameWidth,
+            height: frameHeight,
+            success(res) {
+              // start
+              var startDate = new Date();
+              // process image
+              tracker.track(res.data, frameWidth, frameHeight);
+              // end
+              var endDate = new Date();
+              console.log('cost time:', endDate - startDate, 'ms');
+            }
+          });
+        });
+      });
+
+    // We can process images directly, but it will become slow.
+    // process image directly
+    //tracker.track(resData, imageWidth, imageHeight);
+
   },
   startTacking() {
     var _that = this;
-    var frameData = null;
-    var frameWidth = 0;
-    var frameHeight = 0;
+    var resData = null;
+    var resWidth = 0;
+    var resHeight = 0;
     const context = wx.createCameraContext();
 
     if (!context.onCameraFrame) {
@@ -145,37 +376,35 @@ Page({
       });
       return;
     }
-
     // real-time
-    const listener = context.onCameraFrame(function (frame) {
-      console.log('frame:', frame.width, frame.height);
-      frameData = new Uint8ClampedArray(frame.data);
-      frameWidth = frame.width;
-      frameHeight = frame.height;
+    listener = context.onCameraFrame(function (res) {
+      console.log('frame:', res.width, res.height);
+      resData = new Uint8ClampedArray(res.data);
+      resWidth = res.width;
+      resHeight = res.height;
     });
     // start
     listener.start();
     // process
-    const intervalId = setInterval(function () {
-      console.log('tracking');
-      if (frameData) {
-        _that.tracker.track(frameData,
-          frameWidth,
-          frameHeight);
+    intervalId = setInterval(function () {
+      if (resData) {
+        _that.processPhoto(resData, resWidth, resHeight);
       }
     }, intervalTime);
 
-    _that.setData({
-      listener: listener,
-      intervalId: intervalId,
-    });
   },
   initTracking(type) {
-    if (type === 'color') {
-      this.drawByColor();
+    if (type === 'colorTracker') {
+      this.drawByColorTracker();
     }
-    else if (type === 'face') {
-      this.drawByFace();
+    else if (type === 'faceTracker') {
+      this.drawByFaceTracker();
+    }
+    else if (type === 'imageTracker') {
+      this.drawByImageTracker();
+    }
+    else if (type === 'objectTracker') {
+      this.drawByObjectTracker();
     }
   },
   changeDirection() {
--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -1,39 +1,71 @@
-
 const tracking = require('../../utils/tracking.js')
 const face = require('../../utils/face.js')
+const mouth = require('../../utils/mouth.js')
 const landmarks = require('../../utils/Landmarks.js')
 const regressor = require('../../utils/Regressor.js')
+const ImageTracker = require('../../utils/ImageTracker.js')
 const canvasId = 'canvas1';
+const hiddenCanvasId = 'hiddenCanvas';
+// canvas width
 const canvasWidth = 375;
+// canvas height
 const canvasHeight = 450;
+// small frame size
+const frameWidth = 200;
+// face tracker parameter
+const initialScale = 2;
+// color tracker parameter
+const minDimension = 4;
+// pattern image url: relative url,temp url and network url.
+const patternImageUrl = '../../face.jpg';
+// pattern image width
+const patternFrameWidth = 200;
+// if is taking photo
+var isRunning = true;
+// color tacker, face tracker, image tracker.
+var tracker = null;
+// temp photo path
+var tempImagePath = null;
+// height of frame size
+var frameHeight = 0;
+// temp pattern Image Path
+var tempPatternImagePath = null;
+//pattern image height
+var patternFrameHeight = 0;
 
 Page({
   data: {
     btnText: 'Take a photo',
-    isRunning: true,
-    tracker: null,
-    tempImagePath: null,
     devicePosition: 'back',
   },
   onLoad: function (option) {
     this.initTracking(option.type);
   },
-  drawByColor() {
+  drawByColorTracker() {
     var _that = this;
     const ctx = wx.createCanvasContext(canvasId);
-
-    _that.tracker = new tracking.ColorTracker(['yellow', 'magenta', 'cyan']);
-    _that.tracker.on('track', function (event) {
+    tracker = new tracking.ColorTracker(['yellow', 'magenta', 'cyan']);
+    tracker.setMinDimension(minDimension);
+    tracker.on('track', function (event) {
       if (event.data.length != 0) {
         console.log(event);
       }
       // draw photo on canvas
-      ctx.drawImage(_that.data.tempImagePath, 0, 0, canvasWidth, canvasHeight);
+      ctx.drawImage(tempImagePath, 0, 0, canvasWidth, canvasHeight);
+      // size ratio 
+      var widthRatio = canvasWidth / frameWidth;
+      var heightRatio = canvasHeight / frameHeight;
       // draw tracking points
       event.data.forEach(function (rect) {
         if (rect.color === 'custom') {
-          rect.color = _that.tracker.customColor;
+          rect.color = tracker.customColor;
         }
+        // scale
+        rect.x = Math.round(rect.x * widthRatio);
+        rect.y = Math.round(rect.y * heightRatio);
+        rect.width = Math.round(rect.width * widthRatio);
+        rect.height = Math.round(rect.height * heightRatio);
+        // draw
         ctx.strokeStyle = rect.color;
         ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
         // ctx.font = '11px sans-serif';
@@ -43,7 +75,7 @@ Page({
       ctx.draw();
     });
   },
-  drawByFace() {
+  drawByFaceTracker() {
     var _that = this;
     const ctx = wx.createCanvasContext(canvasId);
     tracking.ViolaJones.classifiers.face = face;
@@ -53,15 +85,15 @@ Page({
     tracking.LBF.maxNumStages = 1;
 
     // new LandmarksTracker
-    _that.tracker = new tracking.LandmarksTracker();
-    _that.tracker.setInitialScale(4);
-    _that.tracker.setStepSize(2);
-    _that.tracker.setEdgesDensity(0.1);
-    _that.tracker.on('track', function (event) {
+    tracker = new tracking.LandmarksTracker();
+    tracker.setInitialScale(initialScale);
+    tracker.setStepSize(2);
+    tracker.setEdgesDensity(0.1);
+    tracker.on('track', function (event) {
       if (!event.data) {
         return;
       }
-
+      console.log(event.data);
       if (event.data.faces.length === 0) {
         var message = 'No results found.';
         console.log(message);
@@ -70,14 +102,20 @@ Page({
           icon: 'none'
         });
       }
-      console.log(event.data);
-
-      // draw photo on canvas
-      ctx.drawImage(_that.data.tempImagePath, 0, 0, canvasWidth, canvasHeight);
-
+      // draw origin photo on canvas
+      ctx.drawImage(tempImagePath, 0, 0, canvasWidth, canvasHeight);
+      // size ratio 
+      var widthRatio = canvasWidth / frameWidth;
+      var heightRatio = canvasHeight / frameHeight;
       // draw rect
       if (event.data.faces) {
         event.data.faces.forEach(function (rect) {
+          // scale
+          rect.x = Math.round(rect.x * widthRatio);
+          rect.y = Math.round(rect.y * heightRatio);
+          rect.width = Math.round(rect.width * widthRatio);
+          rect.height = Math.round(rect.height * heightRatio);
+          // draw
           ctx.strokeStyle = '#a64ceb';
           ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
           //ctx.font = '11px sans-serif';
@@ -91,9 +129,15 @@ Page({
       if (event.data.landmarks) {
         event.data.landmarks.forEach(function (landmarks) {
           for (var landmark in landmarks) {
+            var x = landmarks[landmark][0];
+            var y = landmarks[landmark][1];
+            // scale from 480x600 to 357x500
+            x = Math.round(x * widthRatio);
+            y = Math.round(y * heightRatio);
+            // draw
             ctx.beginPath();
             ctx.fillStyle = "#fff";
-            ctx.arc(landmarks[landmark][0], landmarks[landmark][1], 1, 0, 2 * Math.PI);
+            ctx.arc(x, y, 1, 0, 2 * Math.PI);
             ctx.fill();
           }
         });
@@ -102,50 +146,194 @@ Page({
       ctx.draw();
     });
   },
+  drawByObjectTracker() {
+    var _that = this;
+    const ctx = wx.createCanvasContext(canvasId);
+    tracking.ViolaJones.classifiers.mouth = mouth;
+    tracker = new tracking.ObjectTracker(['mouth']);
+    tracker.setInitialScale(1);
+    tracker.setStepSize(2);
+    tracker.setEdgesDensity(0.1);
+    tracker.on('track', function (event) {
+      if (event.data.length != 0) {
+        console.log(event);
+      }
+      if (event.data.length === 0) {
+        var message = 'No results found.';
+        console.log(message);
+        wx.showToast({
+          title: message,
+          icon: 'none'
+        });
+      }
+      // draw photo on canvas
+      ctx.drawImage(tempImagePath, 0, 0, canvasWidth, canvasHeight);
+      // size ratio 
+      var widthRatio = canvasWidth / frameWidth;
+      var heightRatio = canvasHeight / frameHeight;
+      // draw tracking points
+      event.data.forEach(function (rect) {
+        // scale
+        rect.x = Math.round(rect.x * widthRatio);
+        rect.y = Math.round(rect.y * heightRatio);
+        rect.width = Math.round(rect.width * widthRatio);
+        rect.height = Math.round(rect.height * heightRatio);
+        // draw
+        ctx.strokeStyle = rect.color;
+        ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        // ctx.font = '11px sans-serif';
+        ctx.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+        ctx.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      });
+      ctx.draw();
+    });
+  },
+  getPatternImage(patternImageUrl, callback) {
+    var _that = this;
+    const ctx = wx.createCanvasContext(hiddenCanvasId);
+
+    wx.getImageInfo({
+      src: patternImageUrl,
+      success: function (res) {
+        // size ratio
+        patternFrameHeight = (res.height / res.width) * patternFrameWidth;
+        // for test
+        tempPatternImagePath = res.path;
+        // draw image on canvas
+        ctx.drawImage(res.path, 0, 0, frameWidth, patternFrameHeight);
+        ctx.draw(false, function () {
+          wx.canvasGetImageData({
+            canvasId: hiddenCanvasId,
+            x: 0,
+            y: 0,
+            width: frameWidth,
+            height: patternFrameHeight,
+            success(canvasRes) {
+              if (typeof callback === 'function') {
+                console.log('pattern image', canvasRes.width, canvasRes.height);
+                callback(canvasRes.data, canvasRes.width, canvasRes.height);
+              }
+            }
+          });
+        });
+      },
+      fail: function (error) {
+        console.log('getPatternImage', error);
+      }
+    });
+  },
+  drawByImageTracker() {
+    var _that = this;
+    const ctx = wx.createCanvasContext(canvasId);
+    // get patter image
+    _that.getPatternImage(patternImageUrl, function (patternPixels, width, height) {
+      tracker = new ImageTracker(patternPixels, width, height);
+      tracker.on('track', function (event) {
+        if (event.data != null) {
+          console.log('event.data', event.data);
+        }
+        if (event.data.transform.data.length === 0) {
+          var message = 'No results found.';
+          console.log(message);
+          wx.showToast({
+            title: message,
+            icon: 'none'
+          });
+        }
+        // draw origin photo on canvas
+        ctx.drawImage(tempImagePath, 0, 0, canvasWidth, canvasHeight);
+        // size ratio 
+        var widthRatio = canvasWidth / frameWidth;
+        var heightRatio = canvasHeight / frameHeight;
+
+        if (event.data) {
+          var transformArray = event.data.transform.data;
+          var a = transformArray[0];
+          var c = transformArray[1];
+          var e = transformArray[2];
+          var b = transformArray[3];
+          var d = transformArray[4];
+          var f = transformArray[5];
+          console.log(a, b, c, d, e, f);
+          // transform canvas
+          ctx.transform(a, b, c, d, e* widthRatio, f* heightRatio);
+
+          // draw sample pattern on canvas
+          ctx.fillStyle = 'rgba(0,0,0,0.5)';
+          ctx.fillRect(0, 0, patternFrameWidth * widthRatio, patternFrameHeight * heightRatio);
+        }
+        ctx.draw();
+      });
+    });
+  },
   initTracking(type) {
-    if (type === 'color') {
-      this.drawByColor();
+    if (type === 'colorTracker') {
+      this.drawByColorTracker();
     }
-    else if (type === 'face') {
-      this.drawByFace();
+    else if (type === 'faceTracker') {
+      this.drawByFaceTracker();
     }
+    else if (type === 'imageTracker') {
+      this.drawByImageTracker();
+    }
+    else if (type === 'objectTracker') {
+      this.drawByObjectTracker();
+    }
+  },
+  processPhoto(photoPath, imageWidth, imageHeight) {
+    var _that = this;
+    const ctx = wx.createCanvasContext(hiddenCanvasId);
+    // image position
+    const imageX = 0;
+    const imageY = 0;
+    // size ratio
+    frameHeight = (imageHeight / imageWidth) * frameWidth;
+    // save origin image path
+    tempImagePath = photoPath;
+    // draw image on hidden canvas
+    ctx.drawImage(photoPath, imageX, imageY, frameWidth, frameHeight);
+    // waiting for drawing
+    ctx.draw(false, function () {
+      // get image data from hidden canvas
+      wx.canvasGetImageData({
+        canvasId: hiddenCanvasId,
+        x: imageX,
+        y: imageY,
+        width: frameWidth,
+        height: frameHeight,
+        success(res) {
+          // start
+          var startDate = new Date();
+          // process image
+          tracker.track(res.data, frameWidth, frameHeight);
+          // end
+          var endDate = new Date();
+          console.log('cost time:', endDate - startDate, 'ms');
+        }
+      });
+    });
   },
   takePhoto() {
     var _that = this;
     const context = wx.createCameraContext();
     const ctx = wx.createCanvasContext(canvasId);
-
-    if (_that.data.isRunning) {
+    if (isRunning) {
       _that.setData({
         btnText: 'Retry',
-        isRunning: false,
       });
+      isRunning = false;
       // take a photo
       context.takePhoto({
         quality: 'normal',
         success: (res) => {
           var photoPath = res.tempImagePath;
-          _that.setData({
-            tempImagePath: photoPath,
-          });
-          console.log(photoPath);
-          // draw photo on canvas
-          ctx.drawImage(photoPath, 0, 0, canvasWidth, canvasHeight);
-          // waiting for drawing
-          ctx.draw(false, function () {
-            // get image data from canvas
-            wx.canvasGetImageData({
-              canvasId: canvasId,
-              x: 0,
-              y: 0,
-              width: canvasWidth,
-              height: canvasHeight,
-              success(res) {
-                // process image
-                _that.tracker.track(res.data, canvasWidth, canvasHeight);
-                res = null;
-              }
-            });
+          //get size of image 
+          wx.getImageInfo({
+            src: photoPath,
+            success(res) {
+              console.log('size of image:', res.width, res.height);
+              _that.processPhoto(photoPath, res.width, res.height);
+            }
           });
         }
       });
@@ -153,8 +341,8 @@ Page({
     else {
       _that.setData({
         btnText: 'Take a photo',
-        isRunning: true,
       });
+      isRunning = true;
       // clear canvas
       ctx.draw();
     }
