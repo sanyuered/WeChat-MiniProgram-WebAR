3ec28d0816c7d7aee2ad30cd5294f8456946ae28
AR Fashion <la2040@live.com>
2021-09-07 22:10:49 +0800

    fixed bug

--- package_image_tracker/pages/camera/camera.js
+++ package_image_tracker/pages/camera/camera.js
@@ -1,8 +1,6 @@
 const image = require('../../utils/imageBusiness.js');
 const model = require('../../utils/modelBusiness.js');
 const canvasWebGLId = 'canvasWebGL';
-// throttling for Android
-var cameraFrameMax = 20;
 // a url of sprite image
 const modelUrl = '../../utils/cat_beard.png';
 
@@ -19,8 +17,8 @@ Page({
         const system = wx.getSystemInfoSync().system;
         // if iOS
         if (system.indexOf('iOS') !== -1) {
-            // throttling for iOS
-            cameraFrameMax = 4000;
+              // throttling for iOS
+              this.intervalTimeout = 3000;
         }
     },
     onLoad() {
@@ -44,36 +42,36 @@ Page({
     onCameraFrame_callback(resData,
         canvasWidth,
         canvasHeight) {
- // process start
- image.detect(resData,
-    canvasWidth,
-    canvasHeight,
-    function (event) {
-        var result = event.data;
-        if (result && result.prediction) {
-            // set the rotation and position of the 3d model.    
-            model.setModel(result.prediction,
-                canvasWidth,
-                canvasHeight);
-        } else {
-            var message = 'No results.';
-            wx.showToast({
-                title: message,
-                icon: 'none'
-            });
-        }
+        // process start
+        image.detect(resData,
+            canvasWidth,
+            canvasHeight,
+            function (event) {
+                var result = event.data;
+                if (result && result.prediction) {
+                    // set the rotation and position of the 3d model.    
+                    model.setModel(result.prediction,
+                        canvasWidth,
+                        canvasHeight);
+                } else {
+                    var message = 'No results.';
+                    wx.showToast({
+                        title: message,
+                        icon: 'none'
+                    });
+                }
 
-    });
-// process end
-        },
+            });
+        // process end
+    },
     startTacking() {
         var _that = this;
         const context = wx.createCameraContext();
 
-         // real-time
-         var frameData;
-         var canvasWidth;
-         var canvasHeight;
+        // real-time
+        var frameData;
+        var canvasWidth;
+        var canvasHeight;
         this.listener = context.onCameraFrame(function (res) {
             frameData = new Uint8ClampedArray(res.data);
             canvasWidth = res.width;
